0e8bb3c3178a656b4864d8acb7e3f63391ebedeb
Carla <mcarlaalexia@gmail.com>
2023-01-16 14:12:39 -0300

    arreglo

--- src/main/java/com/example/Springsecurity/WebSecurityConfig.java
+++ src/main/java/com/example/Springsecurity/WebSecurityConfig.java
@@ -2,10 +2,14 @@ package com.example.Springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.firewall.HttpFirewall;
@@ -34,17 +38,23 @@ public class WebSecurityConfig{
 
     //habilita un usuario y contraseña en memoria
     //no se guarda en base de datos
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("nami")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager user = new InMemoryUserDetailsManager();
+        user.createUser(User.withUsername("admin")
+                .password(passwordEncoder().encode("nami"))
+                .roles("admin")
+                .build());
+
+        return user;
 
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     //permite la llamada de puntos, barras etc
     @Bean
